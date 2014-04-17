5080f066de6d307de49bebf15b1dcd6a90bd735a
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2014-04-17 23:04:51 +0900

    Modify HTML2DATA_DIR

--- html/define.php
+++ html/define.php
@@ -1,6 +1,6 @@
 <?php
 /** HTMLディレクトリからのDATAディレクトリの相対パス */
-define('HTML2DATA_DIR', '../data/');
+define('HTML2DATA_DIR', './data/');
 
 /** data/module 以下の PEAR ライブラリを優先的に使用する */
 set_include_path(realpath(dirname(__FILE__) . '/' . HTML2DATA_DIR . 'module') . PATH_SEPARATOR . get_include_path());
