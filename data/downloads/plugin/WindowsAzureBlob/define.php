fe39c62dd38e5bb766d839366f7e6cdadc45451a
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2014-04-17 15:19:52 +0900

    Revert "Add Blob plugin."
    This reverts commit 1d9a825c79a063187f528b240b535e82e96017bf.

--- data/downloads/plugin/WindowsAzureBlob/define.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('AZURE_BLOB_PLUGIN_NAME', 'WindowsAzureBlob');
-define('ENDPOINT_PROTOCOL', 'https');
-define('AZURE_BLOB_ACCOUNT_NAME', $_SERVER['AZURE_BLOB_ACCOUNT_NAME']);
-define('AZURE_BLOB_ACCOUNT_KEY', $_SERVER['AZURE_BLOB_ACCOUNT_KEY']);
-set_include_path(get_include_path() . PATH_SEPARATOR . realpath(dirname( __FILE__)) . '/pear');
