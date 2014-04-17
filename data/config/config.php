ba2f78e0b56ba84abbae306d36294cb984636ffb
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2014-04-17 16:13:00 +0900

    Add config.php

---
+++ data/config/config.php
@@ -0,0 +1,22 @@
+<?php
+define('ECCUBE_INSTALL', 'ON');
+define('ROOT_URLPATH', $_SERVER['ROOT_URLPATH']);
+define('HTTP_URL', 'http://' . $_SERVER['SERVER_NAME'] . ROOT_URLPATH);
+define('HTTPS_URL', 'https://' . $_SERVER['SERVER_NAME'] . ROOT_URLPATH);
+define('DOMAIN_NAME', $_SERVER['DOMAIN_NAME']);
+define('DB_TYPE', $_SERVER['DB_TYPE']);
+define('DB_USER', $_SERVER['DB_USER']);
+define('DB_PASSWORD', $_SERVER['DB_PASSWORD']);
+define('DB_SERVER', $_SERVER['DB_SERVER']);
+define('DB_NAME', $_SERVER['DB_NAME']);
+define('DB_PORT', $_SERVER['DB_PORT']);
+define('ADMIN_DIR', $_SERVER['ADMIN_DIR']);
+define('ADMIN_FORCE_SSL', FALSE);
+define('ADMIN_ALLOW_HOSTS', 'a:0:{}');
+define('AUTH_MAGIC', $_SERVER['AUTH_MAGIC']);
+define('PASSWORD_HASH_ALGOS', 'sha256');
+define('MAIL_BACKEND', $_SERVER['MAIL_BACKEND']);
+define('SMTP_HOST', $_SERVER['SMTP_HOST']);
+define('SMTP_PORT', $_SERVER['SMTP_PORT']);
+define('SMTP_USER', $_SERVER['SMTP_USER']);
+define('SMTP_PASSWORD', $_SERVER['SMTP_PASSWORD']);
